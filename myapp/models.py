5fc4246b346f2b6d1c6f6e697a95b72a7067dbac
Kelvin Puch <kelpucha@gmail.com>
2025-09-15 21:15:02 -0400

    added two new tables to the db

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
