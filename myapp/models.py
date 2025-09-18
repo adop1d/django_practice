4fe16e4af927adbfc5371d4b7dc6ef4ce78fca4c
Kelvin Puch <kelpucha@gmail.com>
2025-09-17 20:49:37 -0400

    small change

--- myapp/models.py
+++ myapp/models.py
@@ -13,6 +13,7 @@ class Task(models.Model):
     description = models.TextField()
     completed = models.BooleanField(default=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title + " - "+ self.project.name +" (" + ("Completed" if self.completed else "Pending") + ")"
